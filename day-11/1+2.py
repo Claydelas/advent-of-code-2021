ba55c64dc2b0479cb63a903c2f3c175a81be25e8
Martin Valchev <mv1u18@soton.ac.uk>
2021-12-15 21:45:09 +0200

    Day 11, part 2

--- day-11/1+2.py
+++ day-11/1+2.py
@@ -13,7 +13,9 @@ with open('./day-11/input.txt', 'r') as f:
             octopi[(x, y)] = int(octopus)
 
     flashes = 0
-    for _ in range(100):
+    steps = 0
+    while True:
+        steps += 1
         flashed = []
         for octopus in octopi:
             octopi[octopus] += 1
@@ -28,7 +30,8 @@ with open('./day-11/input.txt', 'r') as f:
                 continue
 
             octopi[flashing] = 0
-            flashes += 1
+            if steps < 101:
+                flashes += 1
 
             for neighbour in adjacent(*flashing):
                 if neighbour in octopi and octopi[neighbour] != 0:
@@ -36,4 +39,8 @@ with open('./day-11/input.txt', 'r') as f:
                     if octopi[neighbour] > 9:
                         flashed.append(neighbour)
 
+        if all(octopus == 0 for octopus in octopi.values()):
+            break
+
     print(flashes)  # part 1 solution (1725)
+    print(steps)    # part 2 solution (308)
